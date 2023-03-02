a99e02681687f314e33f1b55af372e7e6b8e214c
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-03-02 22:34:09 +0600

    Room Database

--- manage_all_files/src/main/java/com/example/manage_all_files/MainActivity.java
+++ manage_all_files/src/main/java/com/example/manage_all_files/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.manage_all_files;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.os.Build.VERSION.SDK_INT;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.Settings;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CODE = 100;
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        ActivityResultLauncher<Intent> resultLauncher =
+//                registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+//                    @Override
+//                    public void onActivityResult(ActivityResult result) {
+//                        if(result!=null){
+//
+//
+//                        }
+//                    }
+//                });
+//
+//        // Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+////        intent.addCategory(Intent.CATEGORY_OPENABLE);
+////        intent.setType("application/pdf");
+////        intent.putExtra(Intent.EXTRA_TITLE, "Testing.pdf");
+//        Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+//        resultLauncher.launch(intent);
+
     }
 }
