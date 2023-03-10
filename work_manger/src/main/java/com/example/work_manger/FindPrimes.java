c96ea09aa3cdf967b08af4bf32843b95f3d19b8c
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-03-10 19:40:46 +0600

    WorkManager

---
+++ work_manger/src/main/java/com/example/work_manger/FindPrimes.java
@@ -0,0 +1,39 @@
+package com.example.work_manger;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class FindPrimes extends Worker {
+    private long current;
+    private final long max;
+
+    public FindPrimes(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        current = 2;
+        max = 1_000_000;
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        while (current <= max && !isStopped()) {
+            if (isPrime(current)) {
+                Log.i("CurrentPrime:", String.valueOf(current));
+            }
+
+            current++;
+        }
+        if (current > max)
+            return Result.success();
+        else
+            return Result.failure();
+    }
+
+    private boolean isPrime(long current) {
+        return true;
+    }
+}
