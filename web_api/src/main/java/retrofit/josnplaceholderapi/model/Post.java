2aa88f6ede3ad9db01980c4b04af4182e0e4c50f
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-04-15 00:13:16 +0600

    RETROFIT

--- web_api/src/main/java/retrofit/josnplaceholderapi/model/Post.java
+++ web_api/src/main/java/retrofit/josnplaceholderapi/model/Post.java
@@ -18,14 +18,4 @@ public class Post {
             '}';
   }
 
-  public Post(String userId, String id, String title, String body) {
-    this.userId = userId;
-    this.id = id;
-    this.title = title;
-    this.body = body;
-  }
-
-  public Post(){
-
-  }
 }
