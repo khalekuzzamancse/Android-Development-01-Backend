5238599d9c1b60d3014478c38e09bcb44982e629
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-04-14 12:10:26 +0600

    Volley

--- web_api/src/main/java/com/khalekuzzaman/just/cse/web_api/MainActivity.kt
+++ web_api/src/main/java/com/khalekuzzaman/just/cse/web_api/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val myVolley=
             MyVolley.getInstance(this.applicationContext)
-        myVolley.doStringRequest()
+        myVolley.doJsonObjectRequest()
     }
 }
--- web_api/src/main/java/com/khalekuzzaman/just/cse/web_api/volley/MyVolley.java
+++ web_api/src/main/java/com/khalekuzzaman/just/cse/web_api/volley/MyVolley.java
@@ -7,8 +7,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MyVolley {
     private static MyVolley instance;
@@ -21,6 +24,7 @@ public class MyVolley {
             Log.i("RESPONSE_GET", response.toString());
         }
     };
+
     private Response.ErrorListener errorListener = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
@@ -46,6 +50,23 @@ public class MyVolley {
                 responseListener, errorListener);
         queue.add(request);
 
+    }
+
+    public void doJsonObjectRequest() {
+        //defining the callback with lambda
+        Response.Listener<JSONObject> onResponse = res -> {
+            Log.i("RESPONSE_GET_JOSONOBJECT", res.toString());
+        };
+        Response.ErrorListener onError = error -> {
+            Log.i("RESPONSE_GET_JOSONOBJECT", "Error");
+        };
+        final String URL = "https://feeds.npr.org/1019/feed.json";
+        //
+        request =
+                new JsonObjectRequest
+                        (Request.Method.GET, URL, null,
+                        onResponse, onError);
+        queue.add(request);
     }
 
     public void stopRequest() {
